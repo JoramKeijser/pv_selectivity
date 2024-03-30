46fe2f0b87c2bf94849fc0967812e90970fdee6f
Joram <joramkeijser@gmail.com>
2024-03-30 12:37:10 +0000

    Consistent color code CI/CP

--- scripts/fig26_rectification.py
+++ scripts/fig26_rectification.py
@@ -16,10 +16,21 @@ from src.utils import write_excel
 sns.set_context("poster")
 sns.set_palette("colorblind")
 plt.style.use(stylesheet)
+
+# WT cells from Lu et al,: calcium-impermeable
+# KO cells: calcium-permeable
+colors = {
+    "CI-AMPAR": blue,
+    "CP-AMPAR": "black",
+    "WT": blue,
+    "KO": "black",
+}
+alphas = {"CI-AMPAR": 1.0, "CP-AMPAR": 0.8, "WT": 1.0, "KO": 0.8}
+
 # collecting/saving data:
 save_path = "results/Source Data Extended Data Fig. 26.xlsx"
 data_frames = {}
-for panel in ['a', 'b', 'c', 'd', 'e', 'f']:
+for panel in ["a", "b", "c", "d", "e", "f"]:
     data_frames[panel] = pd.DataFrame()
 
 # Load the data
@@ -43,33 +54,45 @@ M_WT = np.max(I_WT / (V_reversal - voltage))
 # Make the figure
 fig, ax = plt.subplots(1, 3, figsize=(4.7, 1.3), sharey=False)
 # The data
-ax[0].text(-60, 0.0, "CI-AMPARs", color="grey")
-ax[0].text(-60, 0.13, "CP-AMPARs", color=blue)
-ax[0].plot(voltage, I_KO, color="grey")
-ax[0].plot(voltage, I_WT, color=blue)
+ax[0].text(-60, 0.13, "CI-AMPARs", color=colors["CI-AMPAR"], alpha=alphas["CI-AMPAR"])
+ax[0].text(-60, 0, "CP-AMPARs", color=colors["CP-AMPAR"], alpha=alphas["CP-AMPAR"])
+ax[0].plot(voltage, I_WT, color=colors["WT"], alpha=alphas["WT"])
+ax[0].plot(voltage, I_KO, color=colors["KO"], alpha=alphas["WT"])
 ax[0].set_xlabel("Membrane potential (mV)")
 ax[0].set_ylabel("Current (norm.)")
 ax[0].set_ylim([-0.05, 1.05])
 ax[0].set_xticks([-60, -40, -20])
 ax[0].set_yticks([0, 0.5, 1])
-data_frames['a']['voltage'] = np.array(voltage)
-data_frames['a']['Current KO'] = np.array(I_KO)
-data_frames['a']['Current WT'] = np.array(I_WT)
+data_frames["a"]["voltage"] = np.array(voltage)
+data_frames["a"]["Current KO"] = np.array(I_KO)
+data_frames["a"]["Current WT"] = np.array(I_WT)
 
 
 # Estimated conductance
-ax[1].text(-60, 0.0, "CI-AMPARs", color="grey")
-ax[1].text(-60, 0.13, "CP-AMPARs", color=blue)
-ax[1].plot(voltage, (I_KO / (V_reversal - voltage) - m) / (M - m), color="grey")
-ax[1].plot(voltage, I_WT / (V_reversal - voltage) / M_WT, color=blue)
+ax[1].text(-60, 0.13, "CI-AMPARs", color=colors["CI-AMPAR"], alpha=alphas["CI-AMPAR"])
+ax[1].text(-60, 0, "CP-AMPARs", color=colors["CP-AMPAR"], alpha=alphas["CP-AMPAR"])
+ax[1].plot(
+    voltage,
+    (I_KO / (V_reversal - voltage) - m) / (M - m),
+    color=colors["KO"],
+    alpha=alphas["KO"],
+)
+ax[1].plot(
+    voltage,
+    I_WT / (V_reversal - voltage) / M_WT,
+    color=colors["WT"],
+    alpha=alphas["WT"],
+)
 ax[1].set_xlabel("Membrane potential (mV)")
 ax[1].set_ylabel("Conductance (norm.)")
 ax[1].set_ylim([-0.05, 1.05])
 ax[1].set_xticks([-60, -40, -20])
 ax[1].set_yticks([0, 0.5, 1])
-data_frames['b']['voltage'] = np.array(voltage)
-data_frames['b']['Conductance KO'] =  np.array((I_KO / (V_reversal - voltage) - m) / (M - m))
-data_frames['b']['Conductance WT'] = np.array(I_WT / (V_reversal - voltage) / M_WT)
+data_frames["b"]["voltage"] = np.array(voltage)
+data_frames["b"]["Conductance KO"] = np.array(
+    (I_KO / (V_reversal - voltage) - m) / (M - m)
+)
+data_frames["b"]["Conductance WT"] = np.array(I_WT / (V_reversal - voltage) / M_WT)
 
 # Scaling functions
 labels = ["0CP + 1CI (GluA2)", "0.8CP + 0.2CI (eGFP)"]
@@ -90,9 +113,13 @@ ax[2].plot(
     label=labels[1],
     color=colors[1],
 )
-data_frames['c']['rate'] = plot_rates
-data_frames['c'][f'scale {labels[0]}'] = scaling_fn(plot_rates, 0, df, I_WT, I_KO, M_WT, M, m)
-data_frames['c'][f'scale {labels[1]}'] = scaling_fn(plot_rates, 0.8, df, I_WT, I_KO, M_WT, M, m)
+data_frames["c"]["rate"] = plot_rates
+data_frames["c"][f"scale {labels[0]}"] = scaling_fn(
+    plot_rates, 0, df, I_WT, I_KO, M_WT, M, m
+)
+data_frames["c"][f"scale {labels[1]}"] = scaling_fn(
+    plot_rates, 0.8, df, I_WT, I_KO, M_WT, M, m
+)
 ax[2].set_xlabel("Rate (1/s)")
 ax[2].set_ylabel("Weight scaling")
 ax[2].set_ylim([-0.05, 1.05])
@@ -110,10 +137,10 @@ for alpha, color in zip([0, 0.8], [green, pink]):
     )
     ax[0].plot(stimuli * 180 / np.pi, rates[-1], color=color)
     ax[1].plot(stimuli * 180 / np.pi, rates[-1] / rates[-1].max(), color=color)
-    data_frames['d']['stimulus'] = stimuli
-    data_frames['d']['rate'] = rates[-1]
-    data_frames['e']['stimulus'] = stimuli
-    data_frames['e']['rate (norm)'] = rates[-1] / rates[-1].max()
+    data_frames["d"]["stimulus"] = stimuli
+    data_frames["d"]["rate"] = rates[-1]
+    data_frames["e"]["stimulus"] = stimuli
+    data_frames["e"]["rate (norm)"] = rates[-1] / rates[-1].max()
 
 ax[0].set_ylim([0, 7])
 ax[1].set_ylim([0, 1.1])
@@ -132,8 +159,8 @@ for i, alpha in enumerate(alphas):
         ax[2].scatter(alpha, osis[i], color=pink)
         print(f"OSI eGFP: {osis[i]:0.2f}")
 ax[2].plot(alphas, osis, color="grey")
-data_frames['f']['alpha'] = alphas
-data_frames['f']['OSI'] = osis
+data_frames["f"]["alpha"] = alphas
+data_frames["f"]["OSI"] = osis
 
 ax[2].set_xlabel(r"CP-AMPAR coefficient $\alpha$")
 ax[-1].set_ylim([0.3, 0.6])
@@ -150,4 +177,4 @@ ax[1].set_ylabel("Rate (norm.)")
 
 fig.tight_layout()
 plt.savefig(figdir + "fig26def_rectification.png", dpi=300)
-write_excel(save_path, data_frames) # save data
+write_excel(save_path, data_frames)  # save data
